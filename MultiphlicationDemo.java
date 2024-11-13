f5ed1db21c9be0e5b08ffe45af4be6837be30341
Sachingupta03 <sachinkumargupta072@gmail.com>
2024-11-12 18:46:23 -1000

    added sum program

---
+++ MultiphlicationDemo.java
@@ -0,0 +1,27 @@
+public class MultiphlicationDemo {
+
+    public void mulNos() {
+        //Inter Multiphlication
+        int a = 10;
+        int b = 20;
+
+        //float Multiplication
+        float no1 = 3.14f;
+        float no2 = 1.13f;
+
+        //double Multiplication
+        double x = 3.1241314;
+        double y = 2.2222263;
+
+        System.out.println("intsum" + (a - b));
+        System.out.println("float sum" + (no1 - no2));
+        System.out.println("doulesum" + (x - y));
+    }
+
+        public static void main(String[]args){
+            MultiphlicationDemo demo = new MultiphlicationDemo();
+            demo.mulNos();
+        }
+
+
+    }
